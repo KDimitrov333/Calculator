e8b9ff3edd751b397b741bd9c2be868bd54f49cf
Kaloyan <kaloyan.sv.dimitrov@gmail.com>
2024-09-03 16:29:51 +0300

    Added functionality to the = and clear buttons

--- logic.js
+++ logic.js
@@ -17,17 +17,13 @@ function divide(a, b) {
 function operate(operator, a, b) {
     switch (operator) {
         case "+":
-            add(a, b);
-            break;
+            return add(a, b);
         case "-":
-            subtract(a, b);
-            break;
+            return subtract(a, b);
         case "*":
-            multiply(a, b);
-            break;
+            return multiply(a, b);
         case "/":
-            divide(a, b);
-            break;
+            return divide(a, b);
     }
 }
 
@@ -37,6 +33,7 @@ let operator;
 let displayVal = "";
 
 const display = document.querySelector("#display");
+
 const numButtons = Array.from(document.querySelectorAll(".num-button"));
 numButtons.forEach(item => {
     item.addEventListener("click", () => {
@@ -44,10 +41,72 @@ numButtons.forEach(item => {
         displayVal = display.textContent;
     })
 });
+
 const opButtons = Array.from(document.querySelectorAll(".operator-button"));
 opButtons.forEach(item => {
     item.addEventListener("click", () => {
-        display.textContent += " " + item.textContent + " ";
-        displayVal = display.textContent;
+        if (display.textContent == "") {
+            alert("You cannot begin with an operator");
+        }
+        else {
+            if (display.textContent.slice(-1) == " ") {
+                alert("You cannot enter an operator after an operator");
+            }
+            else {
+                display.textContent += " " + item.textContent + " ";
+                displayVal = display.textContent;
+            }
+        }
     })
 });
+
+function calc() {
+    if (displayVal.slice(-1) == " ") {
+        displayVal = displayVal.slice(0, -3);
+    }
+
+    let disArr = displayVal.split(" ");
+    let numStrings = [];
+    let operators = [];
+
+    for (let i = 0; i < disArr.length; i++) {
+        if (i % 2 === 0) {
+            numStrings.push(disArr[i]);
+        }
+        else {
+            operators.push(disArr[i]);
+        }
+    }
+
+    let nums = numStrings.map((item) => Number(item));
+
+    while (operators.includes("*") || operators.includes("/")) {
+        let prioIndex = operators.findIndex(item => item == "*" || item == "/");
+        operator = operators.find(item => item == "*" || item == "/");
+        num1 = nums[prioIndex];
+        num2 = nums[prioIndex + 1];
+        console.log(operator);
+        nums.splice(prioIndex, 2, operate(operator, num1, num2));
+        operators.splice(prioIndex, 1);
+    }
+
+    while (nums.length > 1) {
+        operator = operators[0];
+        num1 = nums[0];
+        num2 = nums[1];
+        nums.splice(0, 2, operate(operator, num1, num2));
+        operators.shift();
+    }
+
+    display.textContent = nums[0];
+    displayVal = display.textContent;
+}
+
+const equalsBtn = document.querySelector("#equals");
+equalsBtn.addEventListener("click", calc);
+
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", () => {
+    display.textContent = "";
+    displayVal = display.textContent;
+})
