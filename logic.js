77a96f78a9d6367622a1cc60dbd201e8f7e20b8e
Kaloyan <kaloyan.sv.dimitrov@gmail.com>
2024-09-03 10:27:18 +0300

    Created an operate function

--- logic.js
+++ logic.js
@@ -13,3 +13,26 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
+
+console.log(divide(10, 2));
+
+let num1;
+let num2;
+let operator;
